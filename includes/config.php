e7107bedfc19e017e0a47e5cddfcffb22f19ece7
NeosLab <github@neoslab.com>
2026-06-29 09:09:41 +0200

    Automated sync

--- includes/config.php
+++ includes/config.php
@@ -5,6 +5,8 @@
 $conf = [
     'sitename'  =>  'BlitzSweep',
     'domain'    =>  'blitzsweep.com',
+    'email'     =>  'info@neoslab.com',
+    'mobile'    =>  '',
     'gtag'      =>  '',
     'prod'      =>  true
 ];
